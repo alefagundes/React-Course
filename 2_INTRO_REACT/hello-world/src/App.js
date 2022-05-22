4ee51c8a80c84f9c5a7891b5061b3ed95ac097aa
alefagundes <alefagundes2000.af@gmail.com>
2022-05-22 14:24:11 -0300

    Introduction to react fundamentals

---
+++ 2_INTRO_REACT/hello-world/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello world React</h1>
+            <h2>Emmet funcionou</h2>
+            <p>Vamo dale com os dois pes no react e ser feliz com isso</p>
+        </div>
+    )
+}
+
+export default App
